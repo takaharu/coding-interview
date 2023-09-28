4f9341b5e844772c27a00a979d65bc30609dd6e1
weast24 <t.nishi.aaa@gmail.com>
2023-09-29 01:18:24 +0900

    setup backend

---
+++ api/tests/test_views.py
@@ -0,0 +1,18 @@
+from rest_framework.test import APITestCase
+
+
+class CategoryViewTests(APITestCase):
+    def test_list(self):
+        pass
+
+    def test_retrieve(self):
+        pass
+
+    def test_create(self):
+        pass
+
+    def test_update(self):
+        pass
+
+    def test_destroy(self):
+        pass
